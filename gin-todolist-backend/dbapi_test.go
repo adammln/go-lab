cee5cbf3f4d8a1a36b66c15fbb3dd48efae1e1d9
adammln <gitarbetot@gmail.com>
2022-06-21 06:29:49 +0700

    TEST - add TestGetAllTasks - add Test__DeleteTestingColection for db cleanup after test
    FIX
    - fix wrong taskID during dummy tasks generation
    
    REFACTOR
    - add detailing on error logging
    - change some variable/function names

--- gin-todolist-backend/dbapi_test.go
+++ gin-todolist-backend/dbapi_test.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	// "net/http"
-	"net/http/httptest"
-	"testing"
 	"fmt"
+	"log"
+	"net/http/httptest"
 	"os"
 	"reflect"
+	"testing"
 
+	firestore "cloud.google.com/go/firestore"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
 )
 
 func _getTestContext() *gin.Context {
@@ -21,24 +24,24 @@ func _getTestContext() *gin.Context {
 
 func _getDummyTasks() map[string]Task {
 	/** Helper: Generate 10 dummy tasks
-	- 5 Parent Task (ParentID=nil) 
+	- 5 Parent Task (ParentID=nil)
 	- 5 Subtasks
 	- 1 Parent -> 3 subtasks
 	- 1 Parent -> 2 subtasks
 	- 3 Parent -> 0 subtasks (Subtasks=nil)
 	**/
-	
+
 	// # generate default value
 	var tasks = make(map[string]Task)
 	for i := 1; i <= 10; i++ {
 		tmpID := fmt.Sprintf("task_id_%d", i)
 		tmpTask := Task{
-			ID: tmpID,
-			ParentID: "",
+			ID:        tmpID,
+			ParentID:  "",
 			RankOrder: i,
-			Content: fmt.Sprintf("original content for task_id_%d", i),
+			Content:   fmt.Sprintf("original content for task_id_%d", i),
 			IsChecked: false,
-			Subtasks: nil,	
+			Subtasks:  nil,
 		}
 		tasks[tmpID] = tmpTask
 	}
@@ -53,7 +56,7 @@ func _getDummyTasks() map[string]Task {
 	for i, subtaskId := range subtasks_1 {
 		tmpSubtask := tasks[subtaskId]
 		tmpSubtask.ParentID = "task_id_1"
-		tmpSubtask.RankOrder = i+1
+		tmpSubtask.RankOrder = i + 1
 		tasks[subtaskId] = tmpSubtask
 	}
 
@@ -62,11 +65,11 @@ func _getDummyTasks() map[string]Task {
 	subtasks_5 := []string{"task_id_6", "task_id_7"}
 	tmpTask_5 := tasks["task_id_5"]
 	tmpTask_5.Subtasks = subtasks_5
-	tasks["task_id_1"] = tmpTask_5
+	tasks["task_id_5"] = tmpTask_5
 	for i, subtaskId := range subtasks_5 {
 		tmpSubtask := tasks[subtaskId]
-		tmpSubtask.ParentID =  "task_id_5"
-		tmpSubtask.RankOrder = i+1
+		tmpSubtask.ParentID = "task_id_5"
+		tmpSubtask.RankOrder = i + 1
 		tasks[subtaskId] = tmpSubtask
 	}
 	return tasks
@@ -74,15 +77,15 @@ func _getDummyTasks() map[string]Task {
 
 func TestFirebaseConnection(t *testing.T) {
 	c := _getTestContext()
-	// test firebase app initialization 
+	// test firebase app initialization
 	app, errFirebase := _initFirebaseApp(c)
-	if (errFirebase != nil) {
+	if errFirebase != nil {
 		t.Fatal(errFirebase)
 	}
 
 	// test firestore client connection
 	client, errFirestore := _initFirestoreClient(c, app)
-	if (errFirestore != nil) {
+	if errFirestore != nil {
 		t.Fatal(errFirestore)
 	}
 	client.Close()
@@ -91,10 +94,10 @@ func TestFirebaseConnection(t *testing.T) {
 func TestFirebaseConnectionWrapper(t *testing.T) {
 	c := _getTestContext()
 
-	// test Firebase (Firestore) connection wrapper 
+	// test Firebase (Firestore) connection wrapper
 	db := _initDbConnection(c)
 	if db == nil {
-		t.Fatal("DB Error: db client is nil")
+		t.Fatal("[ERROR] TestFirebaseConnectionWrapper: db client is nil")
 	}
 	db.Close()
 }
@@ -103,19 +106,18 @@ func TestCreateNewTasks(t *testing.T) {
 	c := _getTestContext()
 	collectionID := os.Getenv("FIRESTORE_TEST_DATA_COLLECTION_ID")
 	tasks := _getDummyTasks()
-	for _, task_id := range []string{
-		"task_id_1", "task_id_5", "task_id_6", 
-		"task_id_7", "task_id_8", "task_id_9",
-		"task_id_10",
+	for i, taskID := range []string{
+		"task_id_1", "task_id_5", "task_id_8",
+		"task_id_9", "task_id_10",
 	} {
-		task := tasks[task_id]
+		task := tasks[taskID]
 		_, err := dbCreateTask(
 			c,
 			task.Content,
-			task.RankOrder,
+			i+1,
 			collectionID,
 		)
-		if (err != nil) {
+		if err != nil {
 			t.Fatal(err)
 		}
 	}
@@ -123,90 +125,146 @@ func TestCreateNewTasks(t *testing.T) {
 
 func TestGetAllTasks(t *testing.T) {
 	c := _getTestContext()
-	collection_id := os.Getenv("FIRESTORE_TEST_DATA_COLLECTION_ID")
+	collectionID := os.Getenv("FIRESTORE_TEST_DATA_COLLECTION_ID")
 
-	tasksWrapper := dbGetAllTasks(c)
+	taskWrapper, err := dbGetAllTasks(c, collectionID)
 
-	// # TEST datatype is correct: 
-	// - tasksWrapper == TasksWrapper
-	tTaskWrapper := reflect.TypeOf(taskWrapper)
-	if (tTaskWrapper != reflect.TypeOf(TaskWrapper)) {
-		t.Fatalf(`TypeError: Type of taskWrapper = %s, want %s`, tTaskWrapper, reflect.TypeOf(TaskWrapper))
+	// # TEST err
+	if err != nil {
+		t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: Fail at getting all tasks. %v`, err)
 	}
 
-	// - tasksWrapper.Data -> == map[string]Task
-	tTaskWrapperData := reflect.TypeOf(taskWrapper.Data)
-	if (tTaskWrapperData != reflect.TypeOf(map[string]Task)) {
-		t.Fatalf(`TypeError: Type of taskWrapper.Data = %s, want %s`, tTaskWrapper, reflect.TypeOf(map[string]Task))
+	// # TEST datatype is correct:
+	// - taskWrapper == TaskWrapper
+	var tw TaskWrapper
+	if reflect.TypeOf(*taskWrapper) != reflect.TypeOf(tw) {
+		t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: Type of taskWrapper = %s, want %s`, reflect.TypeOf(*taskWrapper), reflect.TypeOf(tw))
 	}
 
-	// - tasksWrapper.Orders -> == []string
-	tTaskWrapperOrders := reflect.TypeOf(taskWrapper.Orders)
-	if (tTaskWrapperOrders != reflect.TypeOf([]string)) {
-		t.Fatalf(`TypeError: Type of taskWrapper.Data = %s, want %s`, tTaskWrapperOrders, reflect.TypeOf([]string))
+	// - taskWrapper.Data -> == map[string]Task
+	var mpTask map[string]Task
+	if reflect.TypeOf(taskWrapper.Data) != reflect.TypeOf(mpTask) {
+		t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: Type of taskWrapper.Data = %s, want %s`, reflect.TypeOf(taskWrapper.Data), reflect.TypeOf(mpTask))
 	}
-	
+
+	// - taskWrapper.Orders -> == []string
+	var lstr []string
+	if reflect.TypeOf(taskWrapper.Orders) != reflect.TypeOf(lstr) {
+		t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: Type of taskWrapper.Data = %s, want %s`, reflect.TypeOf(taskWrapper.Orders), reflect.TypeOf(lstr))
+	}
+
 	// # TEST query result as wanted
 	// - specify wanted length results:
-	wantedTasksLength := 5
-	actualTasksLengthData := len(tasksWrapper.Data)
-	actualTasksLengthOrders := len(tasksWrapper.Orders)
-	
-	// - check tasksWrapper.Data == 5
-	if actualTasksLengthData != WANTED_TASKS_LENGTH {
+	// actualTasksLengthData := len(taskWrapper.Data)
+
+	// wantedTasksLengthData := 10
+	// actualTasksLengthData := len(taskWrapper.Data)
+	// // - check taskWrapper.Data == 5 (aborted, because Data includes subtasks)
+	// if actualTasksLengthData != wantedTasksLengthData {
+	// 	t.Fatalf(
+	// 		`Result Length of dbGetAllTasks()=>taskWrapper.Data = %d, want %d, error`,
+	// 		actualTasksLengthData,
+	// 		wantedTasksLengthData,
+	// 	)
+	// }
+
+	wantedTasksLengthOrder := 5
+	actualTasksLengthOrders := len(taskWrapper.Orders)
+	// - check len(taskWrapper.Orders) == 5
+	if actualTasksLengthOrders != wantedTasksLengthOrder {
 		t.Fatalf(
-			`Result Length of dbGetAllTasks()=>tasksWrapper.Data = %d, want %d, error`, 
-			actualTasksLengthData, 
-			wantedTasksLength,
+			`[ERROR] dbapi_test.TestGetAllTasks: Result Length of dbGetAllTasks()=>taskWrapper.Orders = %d, want %d.`,
+			actualTasksLengthOrders,
+			wantedTasksLengthOrder,
 		)
 	}
 
-	// - check tasksWrapper.Orders == 5
-	if actualTasksLengthOrders != WANTED_TASKS_LENGTH {
-		t.Fatalf(
-			`Result Length of dbGetAllTasks()=>tasksWrapper.Orders = %d, want %d, error`, 
-			actualTasksLengthOrders, 
-			wantedTasksLength,
-		)
-	}
-
-	// Iteration check
+	// Iteration check for parent tasks and subtasks
 	i := 0
-	for docId, task := range tasksWrapper.Data {
+	// errorMarker := false
+	for docID, task := range taskWrapper.Data {
 		// # TEST ordering is correct: [1-5]
 		if task.RankOrder != i+1 {
-			t.Fatalf(`Ordering error: ordering for task "%s" = %d, want %d`, task.Content, task.RankOrder, i+1)
+			t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: Ordering for task "%s" = %d, want %d`, task.Content, task.RankOrder, i+1)
+			// errorMarker = true
 		}
 
 		// # TEST default attr ID: wanted not "" (empty string)
-		if task.ID == "" {
-			t.Fatalf(`Default value of the attribute 'ID' for task "%s" = "", want %s`, task.Content, task.RankOrder, docId)
+		if task.ID == "" || task.ID != docID {
+			t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: Default value of the attribute 'ID' for task "%s" = %s, want %s`, task.Content, task.ID, docID)
 		}
 
 		// # TEST default attr IsChecked: wanted false
 		if task.IsChecked {
-			t.Fatalf(`Default value of the attribute 'IsChecked' for task "%s" = %t, want false`, task.Content, task.IsChecked)
+			t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: Default value of the attribute 'IsChecked' for task "%s" = %t, want false`, task.Content, task.IsChecked)
 		}
+
 		i++
+	}
+
+	// Iteration check for parent tasks only
+	for i, docID := range taskWrapper.Orders {
+		// # TEST ordering is correct: [1-5]
+		task := taskWrapper.Data[docID]
+		if task.RankOrder != i+1 {
+			t.Fatalf(`[ERROR] dbapi_test.TestGetAllTasks: ordering for task "%s" = %d, want %d`, task.Content, task.RankOrder, i+1)
+		}
 	}
 }
 
-// func TestGetAllTasks(t *testing.T) {
-// 	c := _getTestContext()
-// 	collection_id := os.Getenv("FIRESTORE_TEST_DATA_COLLECTION_ID")
+// =================== DANGER ZONE ========================
+// make sure target collectionID is correct!
+func _deleteCollection(ctx *gin.Context, client *firestore.Client,
+	ref *firestore.CollectionRef, batchSize int) error {
 
-// 	WANTED_TASKS_LENGTH := 10
+	for {
+		// Get a batch of documents
+		iter := ref.Limit(batchSize).Documents(ctx)
+		numDeleted := 0
 
-// 	data := dbGetAllTasks(c)
+		// Iterate through the documents, adding
+		// a delete operation for each one to a
+		// WriteBatch.
+		batch := client.Batch()
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return err
+			}
 
-// 	// check sample data != nil
-// 	if (data == nil) {
-// 		t.Fatal("data query = NULL, want []Task")
-// 	}
+			batch.Delete(doc.Ref)
+			numDeleted++
+		}
 
-// 	// TODO: check if length of data as we wanted
+		// If there are no documents to delete,
+		// the process is over.
+		if numDeleted == 0 {
+			return nil
+		}
 
-// 	// TODO: check if ordering is correct
+		_, err := batch.Commit(ctx)
+		if err != nil {
+			return err
+		}
+	}
+}
 
-// 	// TODO: check if all attributes are available
-// }
+// =================== DANGER ZONE ========================
+// make sure target collectionID is correct!
+func Test__DeleteTestingCollection(t *testing.T) {
+	log.Println("[INFO] dbapi_test._DeleteTestingCollection: Deleting all testing documents and collection...")
+	testCollectionID := os.Getenv("FIRESTORE_TEST_DATA_COLLECTION_ID")
+	ctx := _getTestContext()
+	db := _initDbConnection(ctx)
+	testCollection := db.Collection(testCollectionID)
+	err := _deleteCollection(ctx, db, testCollection, 32)
+	if err != nil {
+		t.Fatal("[ERROR] DeleteTestingCollection: Failed at deleting test collection")
+	}
+	db.Close()
+}
+
+// TODO: Create test for empty task list
