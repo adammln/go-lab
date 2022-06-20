04a92c57f760500774da57e50a55e2eae6b4ec10
adammln <gitarbetot@gmail.com>
2022-06-20 20:04:03 +0700

    Feat: add dbCreateTask()

--- gin-todolist-backend/dbapi.go
+++ gin-todolist-backend/dbapi.go
@@ -4,9 +4,9 @@ package main
 import (
   "log"
 	"os"
-	// "fmt"
 	
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	// "google.golang.org/api/iterator"
 
   firebase "firebase.google.com/go"
@@ -17,6 +17,19 @@ import (
 func _generateUuid() string {
 	newUuid := uuid.New()
 	return newUuid.String()
+}
+
+func _generateNewTask(content string, parentID string, rankOrder int) Task {
+	tmpID := _generateUuid()
+	newTask := Task{
+		ID: tmpID,
+		ParentID: parentID,
+		RankOrder: rankOrder,
+		Content: content,
+		IsChecked: false,
+		Subtasks: nil,
+	}
+	return newTask
 }
 
 func _initFirebaseApp(c *gin.Context) (*firebase.App, error) {
@@ -30,11 +43,28 @@ func _initFirebaseApp(c *gin.Context) (*firebase.App, error) {
 }
 
 func _initFirestoreClient(c *gin.Context, app *firebase.App) (*firestore.Client, error) {
-	client, err := app.Firestore(c)
+	db, err := app.Firestore(c)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	return client, err
+	return db, err
+}
+
+func _initDbConnection(c * gin.Context) *firestore.Client {
+	app, _ := _initFirebaseApp(c)
+	db, _ := _initFirestoreClient(c, app)
+	return db
+}
+
+func dbCreateTask(c *gin.Context, content string, rankOrder int, collectionID string) (*firestore.WriteResult, error) {
+	db := _initDbConnection(c)
+	newTask := _generateNewTask(content, "", rankOrder)
+	wr, err := db.Collection(collectionID).Doc(newTask.ID).Set(c, newTask)
+	if err != nil {
+		log.Fatalf(`Database Error: Can't Create Task Document with content "%s"`, content)
+		return nil, err
+	}
+	return wr, nil 
 }
 
 // func dbGetAllTasks(context *gin.Context) []Task{
